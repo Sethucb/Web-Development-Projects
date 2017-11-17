df33ccbec153f74dc15a4bb4b01ffc716d1f9d6d
Sethu Madhavan <sethu969@gmail.com>
2017-11-16 23:19:19 -0500

    Init project structure

---
+++ submit/web-auth/index.js
@@ -0,0 +1,29 @@
+#!/usr/bin/env node
+
+'use strict';
+
+const mongo = require('mongodb').MongoClient;
+const process = require('process');
+
+const options = require('./options');
+// const users = require('./model/users');
+// const model = require('./model/model');
+// const server = require('./server/server');
+
+const DB_URL = 'mongodb://localhost:27017/users';
+
+const initObj = options.options;
+
+if(typeof initObj !== 'object'){
+	console.error('Undefined command options');
+	process.exit(1);
+}
+
+console.log("CHeck over==");
+// mongo.connect(DB_URL).
+// 	then((db) => users.initUsers(db))
+// 	.then(function(db){
+// 		const model1 = new model.Model(db);
+// 		server.serve(initObj,model1);		
+// 	}).
+// 	catch((e) => console.error(e));
